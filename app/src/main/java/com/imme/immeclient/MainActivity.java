4551dc568cbbca41db509093a13883bd9b4b2482
Devi Chrisman Lase <devichrisman@gmail.com>
2015-11-29 19:50:54 +0700

    ok

--- app/src/main/java/com/imme/immeclient/MainActivity.java
+++ app/src/main/java/com/imme/immeclient/MainActivity.java
@@ -1,13 +1,10 @@
 package com.imme.immeclient;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
