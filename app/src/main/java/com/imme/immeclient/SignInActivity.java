68ee669eacb6154b6cbbbbbed5b90dce6818cedd
Devi Chrisman Lase <devichrisman@gmail.com>
2015-12-09 00:18:22 +0700

    font signUp, signIn updated

--- app/src/main/java/com/imme/immeclient/SignInActivity.java
+++ app/src/main/java/com/imme/immeclient/SignInActivity.java
@@ -1,11 +1,15 @@
 package com.imme.immeclient;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -13,6 +17,31 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        TextView sign_in_textview_sign_up = (TextView) findViewById(R.id.sign_in_textview_sign_up);
+        sign_in_textview_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.imme.immeclient.SignUpActivity");
+                startActivity(intent);
+            }
+        });
+
+        // Start Font
+        Typeface hnLight = Typeface.createFromAsset(getAssets(),
+                "fonts/HelveticaNeue-Light.otf");
+
+        Typeface hbqLight = Typeface.createFromAsset(getAssets(),
+                "fonts/HelveticaBQ-Light.otf");
+
+        EditText sign_in_edittext_email = (EditText) findViewById(R.id.sign_in_edittext_email);
+        sign_in_edittext_email.setTypeface(hnLight);
+
+        EditText sign_in_edittext_password = (EditText) findViewById(R.id.sign_in_edittext_password);
+        sign_in_edittext_password.setTypeface(hnLight);
+
+        TextView sign_in_button_sign_in = (TextView) findViewById(R.id.sign_in_button_sign_in);
+        sign_in_button_sign_in.setTypeface(hbqLight);
 
     }
 
--- app/src/main/java/com/imme/immeclient/SignUpActivity.java
+++ app/src/main/java/com/imme/immeclient/SignUpActivity.java
@@ -1,12 +1,17 @@
 package com.imme.immeclient;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
@@ -24,6 +29,40 @@ public class SignUpActivity extends AppCompatActivity {
         tintManager.setNavigationBarTintEnabled(true);
         // set a custom tint color for all system bars
         tintManager.setTintColor(Color.parseColor("#ff0f99da"));
+
+        TextView sign_up_textview_sign_in = (TextView) findViewById(R.id.sign_up_textview_sign_in);
+        sign_up_textview_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.imme.immeclient.SignInActivity");
+                startActivity(intent);
+            }
+        });
+
+        // Start Font
+        Typeface hnLight = Typeface.createFromAsset(getAssets(),
+                "fonts/HelveticaNeue-Light.otf");
+
+        Typeface hbqLight = Typeface.createFromAsset(getAssets(),
+                "fonts/HelveticaBQ-Light.otf");
+
+        EditText sign_up_edittext_full_name = (EditText) findViewById(R.id.sign_up_edittext_full_name);
+        sign_up_edittext_full_name.setTypeface(hnLight);
+
+        EditText sign_up_edittext_email = (EditText) findViewById(R.id.sign_up_edittext_email);
+        sign_up_edittext_email.setTypeface(hnLight);
+
+        EditText sign_up_edittext_password = (EditText) findViewById(R.id.sign_up_edittext_password);
+        sign_up_edittext_password.setTypeface(hnLight);
+
+        EditText sign_up_edittext_confirm_password = (EditText) findViewById(R.id.sign_up_edittext_confirm_password);
+        sign_up_edittext_confirm_password.setTypeface(hnLight);
+
+        TextView sign_up_edittext_phone_number = (EditText) findViewById(R.id.sign_up_edittext_phone_number);
+        sign_up_edittext_phone_number.setTypeface(hnLight);
+
+        TextView sign_up_button_sign_up = (TextView) findViewById(R.id.sign_up_button_sign_up);
+        sign_up_button_sign_up.setTypeface(hbqLight);
     }
 
 }
