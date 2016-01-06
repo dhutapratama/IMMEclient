74664df8ee7e115b4aecd2df255dc45eb5a70970
Devi Chrisman Lase <devichrisman@gmail.com>
2016-01-06 08:40:49 +0700

    Moring Update

--- app/src/main/java/com/imme/immeclient/SignInActivity.java
+++ app/src/main/java/com/imme/immeclient/SignInActivity.java
@@ -76,8 +76,8 @@ public class SignInActivity extends AppCompatActivity {
         final TextView sign_in_button_sign_up = (TextView) findViewById(R.id.sign_in_button_sign_up);
         sign_in_button_sign_up.setTypeface(hbqLight);
 
-        final TextView dont_have_account = (TextView) findViewById(R.id.dont_have_account);
-        dont_have_account.setTypeface(hbqLight);
+        //final TextView dont_have_account = (TextView) findViewById(R.id.dont_have_account);
+        //dont_have_account.setTypeface(hbqLight);
 
         // sign up destination
         sign_in_button_sign_up.setOnClickListener(new View.OnClickListener() {
