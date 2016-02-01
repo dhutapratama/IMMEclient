ae5aac3b129a416c129258e6b689212f2afd777d
Dhuta Pratama <me@dhutapratama.com>
2016-02-02 00:20:47 +0700

    Update Page profile Account
    Page profile Account test in invite get money page
    try to change background balance, maybe cool with Imlek Theme. hahaha

--- app/src/main/java/com/imme/immeclient/MainActivity.java
+++ app/src/main/java/com/imme/immeclient/MainActivity.java
@@ -425,7 +425,7 @@ public class MainActivity extends AppCompatActivity
             drawer.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    Intent intent = new Intent(getApplicationContext(), InviteGetMoneyActivity.class);
+                    Intent intent = new Intent(getApplicationContext(), RecipientListProfileAccountActivity.class);
                     startActivity(intent);
                 }
             }, 200);
